ad6657b9497c189d848fb8322ba09b34a5615654
Faizan <mohammadfaizan980@gmail.com>
2018-12-08 14:35:56 +0500

    Fixed UI issues and added backend for login MVP validation

---
+++ app/src/main/java/com/whatsclone/muhammadfaizan/travelguard/View/RegistrationActivity.java
@@ -0,0 +1,14 @@
+package com.whatsclone.muhammadfaizan.travelguard.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.whatsclone.muhammadfaizan.travelguard.R;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
