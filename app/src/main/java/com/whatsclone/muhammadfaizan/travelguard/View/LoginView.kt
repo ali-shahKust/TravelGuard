431e87d5318d2eba34b4bbdcdc481c01d687dd59
Faizan <mohammadfaizan980@gmail.com>
2018-12-08 10:06:03 +0500

    Initial commit

---
+++ app/src/main/java/com/whatsclone/muhammadfaizan/travelguard/View/LoginView.kt
@@ -0,0 +1,13 @@
+package com.whatsclone.muhammadfaizan.travelguard.View
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.whatsclone.muhammadfaizan.travelguard.R
+
+class LoginView : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_view)
+    }
+}
