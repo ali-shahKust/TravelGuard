eed651264f90402e64c6ee37e4a5a7d255137412
Faizan <mohammadfaizan980@gmail.com>
2018-12-12 18:08:28 +0500

    Added user profile edit activity

---
+++ app/src/main/java/com/whatsclone/muhammadfaizan/travelguard/EditUserProfile/ActivityEditUserProfile.kt
@@ -0,0 +1,13 @@
+package com.whatsclone.muhammadfaizan.travelguard.EditUserProfile
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.whatsclone.muhammadfaizan.travelguard.R
+
+class ActivityEditUserProfile : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_user_profile)
+    }
+}
