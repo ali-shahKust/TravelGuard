20371ecbdec847cab88ea05511461f20365617a2
Faizan <mohammadfaizan980@gmail.com>
2018-12-17 18:16:00 +0500

    init all views

--- app/src/main/java/com/whatsclone/muhammadfaizan/travelguard/MainScreen/ProfileFragment.kt
+++ app/src/main/java/com/whatsclone/muhammadfaizan/travelguard/MainScreen/ProfileFragment.kt
@@ -6,14 +6,29 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 
 import com.whatsclone.muhammadfaizan.travelguard.R
+import de.hdodenhof.circleimageview.CircleImageView
 
 class ProfileFragment : Fragment() {
 
+    lateinit var imgProfile: CircleImageView
+    lateinit var txtUserName: TextView
+    lateinit var btnRequests: Button
+    lateinit var btnEdit: Button
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         var view: View = inflater.inflate(R.layout.fragment_profile, container, false)
+        initViews(view)
         return view
+    }
+
+    private fun initViews(view: View) {
+        imgProfile = view.findViewById(R.id.imgProfilePicture)
+        txtUserName = view.findViewById(R.id.txtUserName)
+        btnEdit = view.findViewById(R.id.btnEditProfile)
+        btnRequests = view.findViewById(R.id.btnRequests)
     }
 }
