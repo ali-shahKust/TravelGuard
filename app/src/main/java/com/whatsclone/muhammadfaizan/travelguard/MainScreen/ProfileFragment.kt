6549fd2eb63fd5fef1dfc4f309bf6297688bdd9f
Faizan <mohammadfaizan980@gmail.com>
2018-12-17 18:50:42 +0500

    Fixed Issues

--- app/src/main/java/com/whatsclone/muhammadfaizan/travelguard/MainScreen/ProfileFragment.kt
+++ app/src/main/java/com/whatsclone/muhammadfaizan/travelguard/MainScreen/ProfileFragment.kt
@@ -8,6 +8,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.squareup.picasso.Picasso
 
 import com.whatsclone.muhammadfaizan.travelguard.R
 import de.hdodenhof.circleimageview.CircleImageView
@@ -16,19 +22,35 @@ class ProfileFragment : Fragment() {
 
     lateinit var imgProfile: CircleImageView
     lateinit var txtUserName: TextView
+    lateinit var txtUserEmail: TextView
     lateinit var btnRequests: Button
     lateinit var btnEdit: Button
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         var view: View = inflater.inflate(R.layout.fragment_profile, container, false)
         initViews(view)
+        loadData(view)
         return view
     }
 
     private fun initViews(view: View) {
         imgProfile = view.findViewById(R.id.imgProfilePicture)
         txtUserName = view.findViewById(R.id.txtUserName)
+        txtUserEmail = view.findViewById(R.id.txtUserEmail)
         btnEdit = view.findViewById(R.id.btnEditProfile)
         btnRequests = view.findViewById(R.id.btnRequests)
+    }
+
+    private fun loadData(view: View) {
+        FirebaseDatabase.getInstance().getReference("TravelGuard").child("Users").child(FirebaseAuth.getInstance().uid!!).addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {}
+
+            override fun onDataChange(p0: DataSnapshot) {
+                var map : HashMap<String, String> = p0.value as HashMap<String, String>
+                Picasso.get().load(map["image_url"]).into(imgProfile)
+                txtUserName.text = map["user_name"]
+                txtUserEmail.text = map["email"]
+            }
+        })
     }
 }
