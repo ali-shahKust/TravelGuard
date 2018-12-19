3042f08fbc9f1c2d6dcd9aad018fa55534aa6c1b
Faizan <mohammadfaizan980@gmail.com>
2018-12-19 16:51:45 +0500

    Added send request option for the users

--- app/src/main/java/com/whatsclone/muhammadfaizan/travelguard/MainScreen/adapters/PeopleAdapter.kt
+++ app/src/main/java/com/whatsclone/muhammadfaizan/travelguard/MainScreen/adapters/PeopleAdapter.kt
@@ -1,15 +1,21 @@
 package com.whatsclone.muhammadfaizan.travelguard.MainScreen.adapters
 
 import android.content.Context
+import android.support.constraint.ConstraintLayout
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import com.squareup.picasso.Picasso
 import com.whatsclone.muhammadfaizan.travelguard.MainScreen.models.PeopleModel
 import com.whatsclone.muhammadfaizan.travelguard.R
 import de.hdodenhof.circleimageview.CircleImageView
+import es.dmoral.toasty.Toasty
 
 class PeopleAdapter constructor(context: Context, userList: List<PeopleModel>) : RecyclerView.Adapter<PeopleAdapter.PeopleHolder>() {
     var context = context
@@ -32,15 +38,38 @@ class PeopleAdapter constructor(context: Context, userList: List<PeopleModel>) :
 
     override fun onBindViewHolder(p0: PeopleHolder, p1: Int) {
         var obj: PeopleModel = userList.get(p1)
+        if (obj.uid == FirebaseAuth.getInstance().uid!!) {
+            p0.layout.maxHeight = 0
+        }
         p0.txt_user_email.text = obj.email
         p0.txt_user_name.text = obj.user_name
+        p0.img_user.setOnClickListener {
+            addPerson(obj.uid)
+        }
         Picasso.get().load(obj.image_url).into(p0.img_user)
+    }
+
+    private fun addPerson(uid: String) {
+        var map = HashMap<String, String>()
+        map["email"] = FirebaseAuth.getInstance().currentUser!!.email.toString()
+        map["image_url"] = FirebaseAuth.getInstance().currentUser!!.photoUrl.toString()
+        map["user_name"] = FirebaseAuth.getInstance().currentUser!!.displayName.toString()
+        FirebaseDatabase.getInstance().getReference("TravelGuard").child("Users")
+                .child(uid).child("Requests").setValue(map).addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Toasty.success(context, "Request sent", Toast.LENGTH_SHORT, true).show()
+                    } else {
+                        Toasty.error(context, task.exception!!.message.toString(), Toast.LENGTH_LONG, true).show()
+                    }
+                }
     }
 
     inner class PeopleHolder(view: View) : RecyclerView.ViewHolder(view) {
         var txt_user_name: TextView = view.findViewById(R.id.txt_people_name)
         var txt_user_email: TextView = view.findViewById(R.id.txt_people_email)
         var img_user: CircleImageView = view.findViewById(R.id.img_people_profile)
+        var img_add: ImageView = view.findViewById(R.id.img_add_person)
+        var layout: ConstraintLayout = view.findViewById(R.id.people_row_layout)
     }
 
 }
--- app/src/main/java/com/whatsclone/muhammadfaizan/travelguard/MainScreen/models/PeopleModel.kt
+++ app/src/main/java/com/whatsclone/muhammadfaizan/travelguard/MainScreen/models/PeopleModel.kt
@@ -4,4 +4,5 @@ class PeopleModel {
     public var email : String = ""
     public var image_url : String = ""
     public var user_name : String = ""
+    public var uid : String = ""
 }
