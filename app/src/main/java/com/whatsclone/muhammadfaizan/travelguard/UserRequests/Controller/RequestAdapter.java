a7ec4b6d5350f85061da8d5f824c37669621eaeb
Faizan <mohammadfaizan980@gmail.com>
2018-12-20 15:50:45 +0500

    Added code to adapter for requests but code for acceptance and rejection is remaining

--- app/src/main/java/com/whatsclone/muhammadfaizan/travelguard/UserRequests/Controller/RequestAdapter.java
+++ app/src/main/java/com/whatsclone/muhammadfaizan/travelguard/UserRequests/Controller/RequestAdapter.java
@@ -6,18 +6,23 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
 import com.whatsclone.muhammadfaizan.travelguard.R;
 import com.whatsclone.muhammadfaizan.travelguard.UserRequests.Model.RequestModel;
 
 import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class RequestAdapter extends RecyclerView.Adapter<RequestAdapter.RequestHolder> {
 
     private Context context;
     private List<RequestModel> userList;
 
-    public RequestAdapter(Context context, List<RequestModel> userList){
+    public RequestAdapter(Context context, List<RequestModel> userList) {
         this.context = context;
         this.userList = userList;
     }
@@ -31,8 +36,11 @@ public class RequestAdapter extends RecyclerView.Adapter<RequestAdapter.RequestH
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RequestHolder requestHolder, int i) {
-
+    public void onBindViewHolder(@NonNull RequestHolder holder, int position) {
+        RequestModel model = userList.get(position);
+        holder.requestName.setText(model.user_name);
+        holder.requestEmail.setText(model.email);
+        Picasso.get().load(model.image_url).into(holder.imgRequest);
     }
 
     @Override
@@ -45,10 +53,18 @@ public class RequestAdapter extends RecyclerView.Adapter<RequestAdapter.RequestH
         return position;
     }
 
-    class RequestHolder extends RecyclerView.ViewHolder
-    {
+    class RequestHolder extends RecyclerView.ViewHolder {
+        CircleImageView imgRequest;
+        TextView requestName, requestEmail;
+        ImageView imgAccept, imgReject;
 
         public RequestHolder(@NonNull View itemView) {
             super(itemView);
+            imgRequest = itemView.findViewById(R.id.img_request);
+            requestName = itemView.findViewById(R.id.txt_name);
+            requestEmail = itemView.findViewById(R.id.txt_email);
+            imgAccept = itemView.findViewById(R.id.img_accept);
+            imgReject = itemView.findViewById(R.id.img_reject);
         }
-    }}
+    }
+}
