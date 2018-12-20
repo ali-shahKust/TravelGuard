c6587cd1af69273a1156e03fd1d414d2dda8716a
Faizan <mohammadfaizan980@gmail.com>
2018-12-20 15:19:12 +0500

    Added request row design file

--- app/src/main/java/com/whatsclone/muhammadfaizan/travelguard/UserRequests/Controller/RequestAdapter.java
+++ app/src/main/java/com/whatsclone/muhammadfaizan/travelguard/UserRequests/Controller/RequestAdapter.java
@@ -1,4 +1,50 @@
 package com.whatsclone.muhammadfaizan.travelguard.UserRequests.Controller;
 
-public class RequestAdapter {
-}
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.whatsclone.muhammadfaizan.travelguard.UserRequests.Model.RequestModel;
+
+import java.util.List;
+
+public class RequestAdapter extends RecyclerView.Adapter<RequestAdapter.RequestHolder> {
+
+    private Context context;
+    private List<RequestModel> userList;
+
+    public RequestAdapter(Context context, List<RequestModel> userList){
+        this.context = context;
+        this.userList = userList;
+    }
+
+    @NonNull
+    @Override
+    public RequestHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RequestHolder requestHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return position;
+    }
+
+    class RequestHolder extends RecyclerView.ViewHolder
+    {
+
+        public RequestHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }}
--- app/src/main/java/com/whatsclone/muhammadfaizan/travelguard/UserRequests/Model/RequestModel.java
+++ app/src/main/java/com/whatsclone/muhammadfaizan/travelguard/UserRequests/Model/RequestModel.java
@@ -1,4 +1,9 @@
 package com.whatsclone.muhammadfaizan.travelguard.UserRequests.Model;
 
 public class RequestModel {
+    public String email;
+    public String image_url;
+    public String user_name;
+
+    public RequestModel(){}
 }
