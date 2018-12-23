d5b50a219d43e1a492e60698d13c75ded192b1b9
Faizan <mohammadfaizan980@gmail.com>
2018-12-23 15:40:52 +0500

    Added friends list MVC architecture but most of the code is still remaining

---
+++ app/src/main/java/com/whatsclone/muhammadfaizan/travelguard/MainScreen/adapters/FriendsAdapter.java
@@ -0,0 +1,51 @@
+package com.whatsclone.muhammadfaizan.travelguard.MainScreen.adapters;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.whatsclone.muhammadfaizan.travelguard.MainScreen.models.FriendsModel;
+
+import java.util.List;
+
+public class FriendsAdapter extends RecyclerView.Adapter<FriendsAdapter.FriendsHolder> {
+
+
+    private List<FriendsModel> friendsList;
+    private Context context;
+
+    FriendsAdapter(List<FriendsModel> friendsList, Context context) {
+        this.context = context;
+        this.friendsList = friendsList;
+    }
+
+    @NonNull
+    @Override
+    public FriendsHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FriendsHolder friendsHolder, int i) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return position;
+    }
+
+    class FriendsHolder extends RecyclerView.ViewHolder {
+        FriendsHolder(View view) {
+            super(view);
+
+        }
+    }
+}
---
+++ app/src/main/java/com/whatsclone/muhammadfaizan/travelguard/MainScreen/models/FriendsModel.java
@@ -0,0 +1,12 @@
+package com.whatsclone.muhammadfaizan.travelguard.MainScreen.models;
+
+public class FriendsModel {
+    public String email;
+    public String uid;
+    public String image_url;
+    public String user_name;
+
+    public FriendsModel(){
+
+    }
+}
