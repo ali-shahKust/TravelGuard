cae751fc0f326533fb2212cd484845537cf51f5c
Faizan <mohammadfaizan980@gmail.com>
2018-12-25 15:38:49 +0500

    Added location permission check

--- app/src/main/java/com/whatsclone/muhammadfaizan/travelguard/LocationSettingsActivity.kt
+++ app/src/main/java/com/whatsclone/muhammadfaizan/travelguard/LocationSettingsActivity.kt
@@ -1,35 +1,83 @@
 package com.whatsclone.muhammadfaizan.travelguard
 
-import android.support.v7.app.AppCompatActivity
+import android.content.DialogInterface
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.widget.CheckBox
 import android.widget.Toast
 
 class LocationSettingsActivity : AppCompatActivity() {
 
-    private lateinit var mToolbar : Toolbar
-    private lateinit var locationCheckBox : CheckBox
+    private lateinit var mToolbar: Toolbar
+    private lateinit var locationCheckBox: CheckBox
+    private var perm = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_location_settings)
         initViews()
+        checkPerm()
+
         locationCheckBox.setOnClickListener { view ->
-            setState()
+            if (!perm) {
+                locationCheckBox.isEnabled = false
+            } else {
+                setState()
+            }
+
         }
     }
 
-    private fun initViews(){
+    private fun initViews() {
         mToolbar = findViewById(R.id.mToolbar)
         locationCheckBox = findViewById(R.id.location_check_box)
     }
-    private fun setState(){
+
+    private fun setState() {
         if (locationCheckBox.isChecked) {
             //TODO : start location service
             Toast.makeText(this@LocationSettingsActivity, "Checked", Toast.LENGTH_SHORT).show()
         } else {
             //TODO : Stop location service
             Toast.makeText(this@LocationSettingsActivity, "Un_Checked", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun checkPerm() {
+        var permArray = arrayOfNulls<String>(2)
+        permArray[0] = android.Manifest.permission.ACCESS_COARSE_LOCATION
+        permArray[1] = android.Manifest.permission.ACCESS_FINE_LOCATION
+
+        if (ContextCompat.checkSelfPermission(this@LocationSettingsActivity, permArray[0].toString()) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this@LocationSettingsActivity, permArray[1].toString()) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this@LocationSettingsActivity, permArray, 69)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 69 && grantResults.isNotEmpty()) {
+            for (permission in grantResults) {
+                if (permission == PackageManager.PERMISSION_DENIED) {
+                    var dialog = AlertDialog.Builder(this@LocationSettingsActivity, R.style.AlertDialogCustom)
+                    dialog.setTitle("Warning")
+                    dialog.setMessage("You will not be able to share your location if location permissions are denied")
+                    dialog.setPositiveButton("Close", DialogInterface.OnClickListener { dlg, which ->
+                        dlg.dismiss()
+                    })
+
+                    dialog.show()
+                    perm = false
+                    return
+                } else {
+                    perm = true
+                }
+            }
         }
     }
 }
